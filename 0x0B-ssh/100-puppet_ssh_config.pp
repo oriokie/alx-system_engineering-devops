f0d246f281a973c04823a36a38cba71ac6dab4c8
“Edwin <oriokie@icloud.com>
2024-02-25 22:15:30 +0300

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
-file { '/etc/ssh/ssh_config':
-  ensure => present,
+file_line { 'Declare_identity_file':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
 }
--> exec { 'Append IdentityFile':
-  command => '/usr/bin/echo "    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
-}
--> exec { 'Append PasswordAuthentication':
-  command => '/usr/bin/echo "    PasswordAuthentication no" >> /etc/ssh/ssh_config'
+
+file_line { 'Turn_off_passwd_auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
 }
