63668838744fdf758bb3c426ce46aa398bc34763
Edwin K. Orioki <edwin.orioki@gmail.com>
2024-02-25 14:39:24 -0500

    fixed with puppet-lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,7 @@ package {'Flask':
 }
 
 package {'Werkzeug':
-	ensure 	 => '2.1.1',
-	provider => 'pip3',
-	require  => Package['Flask'],
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
 }
