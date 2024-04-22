31cb406a27225b08498efeddae2f3ded00ab6857
“Edwin <oriokie@icloud.com>
2024-04-22 20:21:38 +0300

    added json file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -3,8 +3,10 @@
 Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress
 """
+import json
 import requests
 from sys import argv
+
 
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
@@ -12,7 +14,10 @@ if __name__ == "__main__":
     user = requests.get(url + 'users/{}'.format(user_id)).json()
     todos = requests.get(url + 'todos', params={'userId': user_id}).json()
     completed = [task.get('title') for task in todos if task.get('completed')]
-    print('Employee {} is done with tasks({}/{}):'.format(user.get('name'),
-                                                          len(completed),
-                                                          len(todos)))
-    [print('\t {}'.format(task)) for task in completed]
+
+    with open('{}.json'.format(user_id), 'w') as file:
+        json.dump({user_id: [{
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": user.get('username')
+        } for task in todos]}, file)
