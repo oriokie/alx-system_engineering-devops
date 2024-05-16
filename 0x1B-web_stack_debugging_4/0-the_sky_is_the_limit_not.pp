09d67e1886aee91a9c0a6fa2c3226a1bd51f9e39
“Edwin <oriokie@icloud.com>
2024-05-16 09:09:18 +0300

    fixed puppet-lint error

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,7 +5,7 @@
 exec { 'increasing-the-ULIMIT':
     command => 'sed -i "s/15/4096/" /etc/default/nginx',
     path    => '/usr/local/bin:/bin/'
-} ->
+}
 
 # Restart Nginx
 exec { 'restart-nginx-service':
